b09192d5da3a6f69d4e89ed6d5e326fbc6eba457
Luca Mattei <68744118+Luchil-v1@users.noreply.github.com>
2022-03-05 19:21:53 +0100

    page name changes

--- typing/js/paragraphs.js
+++ typing/js/paragraphs.js
@@ -5,7 +5,7 @@ const paragraphs = [
     "trentini torrente verde sus cammello quadro nano becero sbordato ponte mappa verme cancello sasso festa ricco macchina hotel obitorio zanzara uccello uscita truppa poema usciere battello bicchiere yogurt",
     "carota cavallo tavolo quadro penna poema ricotta porta lavapiatti mattone lampone lavagna ovale luce scuola oceano macchina gratuito obitorio probabilmente intenzione uscita terra interesse terra usciere battello ",
     "monitor articolo cubo nocciola alveare cartina giardino letto cammello dromedario lavatrice verme righello pane formaggio testa polo orologio rastrello camaleonte sono sovrano trono luce traversa pallone nitrito",
-    "fuoco lavaggio finestra automobile tenda entrata documento lampone piatrella sedia cavalletto rapace trovare flauto valorant tastiera riquadro parabrezza nano mitico mitologia vaccino trattore farfalla idrante forno",
+    "fuoco lavaggio finestra automobile tenda entrata documento lampone piastrella sedia cavalletto rapace trovare flauto valorant tastiera riquadro parabrezza nano mitico mitologia vaccino trattore farfalla idrante forno",
     "amongus volpone tavolo immobile marinaio proiettore corda albero lavandino gomito legamento cavalletto vetro specchio locale terrario catastrofe orbita surry petra lupestre sono varcare inferno vento gas posate",
     "capretto randomico algoritmo carattere tana rifugio acqua sveglia maniglia ritorna oggetto parentesi piede formato libro immagini riflesso quadrato colore matricola studente anno essere cavo resistenza moto lingua",
 ];
