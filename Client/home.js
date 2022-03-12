449d9cb25c6cb82a5cc7cf743dca586f6a550389
Luca Mattei <68744118+Luchil-v1@users.noreply.github.com>
2022-03-12 18:45:47 +0100

    fixes

--- Client/home.js
+++ Client/home.js
@@ -5,15 +5,6 @@ function displayTime() {
     var sec = dateTime.getSeconds();
     var session = document.getElementById('session');
 
-    if (hrs >= 12) {
-        session.innerHTML = 'PM';
-    } else {
-        session.innerHTML = 'AM';
-    }
-
-    if (hrs > 12) {
-        hrs = hrs - 12;
-    }
 
     document.getElementById('hours').innerHTML = hrs;
     document.getElementById('minutes').innerHTML = min;
