3151e59665934388a5aa82c50133f150e2d3b8a4
Martin Gilmozzi <74062032+Marbanz@users.noreply.github.com>
2022-03-12 15:15:16 +0100

    Modifiche alla webchat

--- Client/webchat/script.js
+++ Client/webchat/script.js
@@ -5,24 +5,39 @@ const messageInput = document.getElementById('message-input');
 const serverip = location.host.split(':');
 const ws = new WebSocket("ws://" + serverip[0] + ":8069");
 
-const name = prompt('What is your name?');
+const password = "SUS Hub(beta)";
+const pass = prompt('Enter the password!');
+if (password == pass) {
+    const name = prompt('What is your name?');
+    if (name == null || name == "") {
+        appendMessage("You didn't set your name correctly")
+    } else {
+        appendMessage("Connecting to the server");
 
-ws.onopen = function() {
-    console.log("Connected to Server");
-    appendMessage('You joined');
-};
+        ws.onopen = function() {
+            console.log("Connected to Server");
+            appendMessage('Connected');
+        };
 
-ws.onmessage = function({ data }) {
-    appendMessage(`${data}`);
-};
+        ws.onmessage = function({ data }) {
+            appendMessage(`${data}`);
+        };
 
-messageForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const message = messageInput.value;
-    ws.send(`${name}: ${message}`);
-    appendMessage(`You: ${message}`);
-    messageInput.value = '';
-});
+        messageForm.addEventListener('submit', e => {
+            e.preventDefault();
+            const message = messageInput.value;
+            if (message.length < 500 && message.length > 0 && message != " ") {
+                ws.send(`${name}: ${message}`);
+                appendMessage(`You: ${message}`);
+                messageInput.value = '';
+            } else {
+                appendMessage("The message does not respect the requirements")
+            }
+        });
+    }
+} else {
+    appendMessage("Wrong password!");
+}
 
 function appendMessage(message) {
     const messageElement = document.createElement('div');
