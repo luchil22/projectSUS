1b1afc4c7f531b59cada9e24c0c02968bc92c856
Martin Gilmozzi <74062032+Marbanz@users.noreply.github.com>
2022-03-14 17:50:42 +0100

    Added webchat autoscroll

--- Client/webchat/script.js
+++ Client/webchat/script.js
@@ -3,51 +3,58 @@ const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 const maxLength = 469;
 
-
 const serverip = location.host.split(':');
 const ws = new WebSocket("ws://" + serverip[0] + ":8069");
 
-const pass = prompt('Enter the password!');
-if (password == pass) {
-    const name = prompt('What is your name?');
-    if (name == null || name == "") {
-        appendMessage("You didn't set your name correctly")
-    } else {
-        appendMessage("Connecting to the server");
-
-        ws.onopen = function() {
-            console.log("Connected to Server");
-            appendMessage('Connected');
-        };
-
-        ws.onmessage = function({ data }) {
-            appendMessage(`${data}`);
-        };
-
-        messageForm.addEventListener('submit', e => {
-
-            e.preventDefault();
-            const message = messageInput.value;
-            if (message.length < maxLength && message.length > 0 && message != " ") {
-
-                ws.send(`${name}: ${message}`);
-                appendMessage(`You: ${message}`);
-                messageInput.value = '';
-
-            } else if (message.length == 0) {
-                alert("Il messaggio deve contenere alemeno un carattere");
-                t
-            } else {
-                alert("Hai superato il limite di " + maxLength + " caratteri")
-            }
-        });
-    }
+const name = prompt('What is your name?');
+if (name == null || name == "") {
+    alert("You didn't set your name correctly")
 } else {
-    appendMessage("Wrong password!");
+    appendMessage("Connecting to the server");
+
+    ws.onopen = function() {
+        appendMessage('Connected');
+    };
+
+    ws.onmessage = function({ data }) {
+        appendMessage(`${data}`);
+    };
+
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const message = messageInput.value;
+        if (message.length < maxLength && message.length > 0) {
+            ws.send(`${name}: ${message}`);
+            appendMessage(`You: ${message}`);
+            messageInput.value = '';
+        } else if (message.length == 0) {
+            alert("Il messaggio deve contenere alemeno un carattere");
+        } else if (message != " ") {
+            alert("Il messaggio deve contenere delle lettere");
+        } else {
+            alert("Hai superato il limite di " + maxLength + " caratteri")
+        }
+    });
 }
+
+let btn = document.getElementById('btn');
+var autoscroll = true;
+btn.onclick = function() {
+    if (autoscroll) {
+        autoscroll = false;
+        alert("Autoscroll disabled");
+    } else {
+        autoscroll = true;
+        alert("Autoscroll enabled");
+        window.scrollBy(0, window.innerHeight);
+    }
+};
 
 function appendMessage(message) {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageContainer.append(messageElement);
+    if (autoscroll) {
+        window.scrollBy(0, window.innerHeight);
+    }
 }
