a2883a7156b2008d99bfab870ba5353ea538e204
Luca Mattei <68744118+Luchil-v1@users.noreply.github.com>
2022-03-14 16:42:32 +0100

    modifiche

--- Client/webchat/script.js
+++ Client/webchat/script.js
@@ -26,12 +26,15 @@ if (password == pass) {
         };
 
         messageForm.addEventListener('submit', e => {
+
             e.preventDefault();
             const message = messageInput.value;
             if (message.length < maxLength && message.length > 0 && message != " ") {
+
                 ws.send(`${name}: ${message}`);
                 appendMessage(`You: ${message}`);
                 messageInput.value = '';
+
             } else if (message.length == 0) {
                 alert("Il messaggio deve contenere alemeno un carattere");
                 t
